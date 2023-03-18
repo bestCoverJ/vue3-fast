7ecb9fc81a8fb697e4d491d22afcc0e1983295e3
coverj <win8yh1@outlook.com>
2023-03-18 18:18:08 +0800

    项目初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            files: ['src/view/index.vue','src/view/**/index.vue'],   // 匹配views和二级目录中的index.vue
+            rules: {
+                'vue/multi-word-component-names':"off",
+            } //给上面匹配的文件指定规则
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+
+const routes = [
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('@/view/index.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
