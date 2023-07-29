d32a72944d410739c23cc1de4763d5f61b85500c
coverj009 <win8yh1@outlook.com>
2023-07-30 02:15:13 +0800

    添加prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential'],
+  overrides: [
+    {
+      files: ['src/view/index.vue', 'src/view/**/index.vue'], // 匹配views和二级目录中的index.vue
+      rules: {
+        'vue/multi-word-component-names': 'off',
+      }, //给上面匹配的文件指定规则
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            files: ['src/view/index.vue','src/view/**/index.vue'],   // 匹配views和二级目录中的index.vue
-            rules: {
-                'vue/multi-word-component-names':"off",
-            } //给上面匹配的文件指定规则
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'vue'],
+  rules: {
+    'linebreak-style': [0, 'error', 'windows'],
+  },
 }
